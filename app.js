dee0c72a594e81acf5ad2b2428d97a478fe8f6d2
CheezeSlayer <hchang1994@hotmail.com>
2019-01-20 11:13:55 -0800

    fixed dependency

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var request = require('request');
+//var request = require('request');
 var app = express();
 
 //1301%20lombard%20street%20philadelphia
 var apiKey = "19A032Hm0hCsHK4F6phJdyWGMa4ftXx1";
-
+/*
 request({
   url: `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey}&location=6555 Wade Road`,
   json: true
@@ -15,7 +15,7 @@ request({
   console.log(response.statusCode);
   console.log(body.results[0].locations[0].latLng);
 });
-
+*/
 
 app.use(express.static(__dirname + '/public'));
 
